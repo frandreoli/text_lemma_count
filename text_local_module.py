3cfb5e200626e8787740069e74c429e1c81e008f
frandreoli <fra.andreoli@hotmail.it>
2024-11-21 21:09:06 +0100

    Added automatic language detection and corrected the appearance of empty string as words

--- text_local_module.py
+++ text_local_module.py
@@ -2,7 +2,7 @@
 #
 def replace_dict_func(language : str = ""):
         if language.lower() == "it":
-            return {"all":"al", "dell":"del", "l":"lo", "sull":"su","nell":"in"}
+            return {"all":"al", "dell":"del", "l":"lo", "sull":"su","nell":"in","d":"di","c":"ci","s":"si","t":"ti","vent":"venti"}
         else:
             return dict({})
 #
@@ -10,7 +10,7 @@ def replace_dict_func(language : str = ""):
 class text_analyzer:
     #    
     #Initialization
-    def __init__ (self, text : str, language : str = "it", verbose : bool = True):
+    def __init__ (self, text : str, language = None, *,verbose : bool = True):
         #
         #Initializing the hidden attributes
         ##Read-only attribute
@@ -31,15 +31,28 @@ class text_analyzer:
         self.__text_counted_dataframe = None #Needs __text_splitted
         self.__text_lemmatized        = None #Needs __text_counted
         self.__text_lemma_counted     = None #Needs __text_lemmatized
-        self.__language_used          = '' 
+        self.__language_used          = self.language 
+        #
+        self._languages = ['ast', 'bg', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'en', 'enm', 'es', 'et', 'fa', 
+                                'fi', 'fr', 'ga', 'gd', 'gl', 'gv', 'hbs', 'hi', 'hu', 'hy', 'id', 'is', 'it', 
+                                'ka', 'la', 'lb', 'lt', 'lv', 'mk', 'ms', 'nb', 'nl', 'nn', 'pl', 'pt', 'ro', 
+                                'ru', 'se', 'sk', 'sl', 'sq', 'sv', 'sw', 'tl', 'tr', 'uk']
     #
     #Retrieve the raw text without allowing for changes
     @property
     def text_raw(self):
-        return self._text
+        return self._text_raw
     @text_raw.setter
     def text_raw(self, value):
-        raise AttributeError("The raw test that is initialized is read-only and cannot be modified.")
+        raise AttributeError("WARNING: The initial raw test is read-only and cannot be modified.")
+    #
+    #Retrieve the list of available languages
+    @property
+    def languages(self):
+        return self._languages
+    @languages.setter
+    def languages(self, value):
+        raise AttributeError("WARNING: The list of available languages is read-only and cannot be modified.")
     #
     #A method to check if an attribute has already been defined
     def __none_check(self,variable):
@@ -54,7 +67,7 @@ class text_analyzer:
             try:
                 import simplemma
             except Exception as exc:
-                print("Error while importing the simplemma library.\nI cannot lemmatize the words.\nError code: ", exc)
+                print("ERROR: Error while importing the simplemma library.\nI cannot lemmatize the words.\nError code: ", exc)
                 return True
         return False
     #
@@ -63,11 +76,11 @@ class text_analyzer:
         global pd
         if "pd" not in globals():
             if self.verbose:
-                print("I need pandas. I'm executing 'import pandas'.")
+                print("I need pandas. I'm executing 'import pandas as pd'.")
             try:
                 import pandas as pd
             except Exception as exc:
-                print("Error while importing the pandas library.\nI cannot lemmatize the words.\nError code: ", exc)
+                print("ERROR: Error while importing the pandas library.\nI cannot lemmatize the words.\nError code: ", exc)
                 return True
         return False
     #
@@ -92,6 +105,7 @@ class text_analyzer:
                 self.__text_processed = self.__text_processed.replace(puntuaction,"")
             #
             self.__text_processed = re.sub(r'\s+', ' ', self.__text_processed)
+            self.__text_processed = self.__text_processed.strip()
             #
         return self.__text_processed
     #
@@ -103,6 +117,8 @@ class text_analyzer:
             if self.verbose:
                 print("Please wait: "+"I am splitting the words.")
             self.__text_splitted = re.split(r" |'", self.__text_processed)
+            #self.__text_splitted = list(filter(lambda x: x != "", self.__text_splitted))
+            self.__text_splitted = [item for item in self.__text_splitted if (not item.isdigit()) and item!=""]
         #
         return self.__text_splitted
     #   
@@ -144,6 +160,22 @@ class text_analyzer:
         #
         if self.__none_check(self.__text_counted):
             self.word_count()  
+        #
+        if type(self.language)==type(None):
+            if self.verbose:
+                print("WARNING: no language is specified, I'll try to detect the language.")
+            max_score = 0.0
+            language_guess = ''
+            text_length = min(3*(10**3), len(self._text_raw))
+            for my_lang in self._languages:
+                
+                score = simplemma.in_target_language(self._text_raw[0:text_length], lang = my_lang)
+                if score>max_score:
+                    max_score = score
+                    language_guess = my_lang
+            print(f"WARNING: the language detected is {language_guess}, with confidence {max_score} (between 0 and 1)")
+            self.language = language_guess
+            self.__language_used = language_guess
         #
         language_check = (self.__language_used != self.language)
         #
@@ -188,7 +220,7 @@ class text_analyzer:
             if self.verbose:
                 print("Please wait: "+"I am counting the lemmas.")
             lemma_occurrences = self.__text_lemmatized.groupby("Lemma")["Raw"].unique().reset_index()
-            lemma_occurrences.columns = ['Lemma', 'Occurrence']
+            lemma_occurrences.columns = ['Lemma', 'Occurrences']
             lemma_counts = self.__text_lemmatized.groupby("Lemma")["Raw Count"].sum().reset_index()
             lemma_counts.columns = ['Lemma', 'Lemma Count']
             self.__text_lemma_counted = lemma_counts.merge(lemma_occurrences, on = "Lemma")    
@@ -200,6 +232,6 @@ class text_analyzer:
             display(self.word_count())
         else:
             if type(word)!=str:
-                print("The word mut be a string.")
+                print("WARNING: The word mut be a string.")
             else:
                 print("The word '"+word+"' occurs",self.word_count(word),"times.")
